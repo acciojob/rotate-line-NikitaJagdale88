f5f2e7cacf6b644e0986f01fed5b86380fcb7e15
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-07-10 20:01:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-//your JS code here. If required.
- var line = document.getElementById("line");
-        var rotation = 0;
 
-        function rotateLine() {
-            rotation += 1;
-            line.style.transform = "rotate(" + rotation + "deg)";
-            requestAnimationFrame(rotateLine);
-        }
-
-        rotateLine();
+    
